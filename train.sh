db70471e9adc549a4027c7a4625b35adf706f161
Anderson Tavares <andersonrochatavares@gmail.com>
2019-07-18 16:26:23 -0300

    adding config, train script and log4j configuration

---
+++ train.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# compiles project
+ant
+
+# configures classpath and runs
+classpath=.:bin:lib/*
+
+echo "Launching experiment..."
+
+java -classpath $classpath -Djava.library.path=lib/ main.Train "$@" 
+
+echo "Done."
