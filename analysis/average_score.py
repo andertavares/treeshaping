deb7c29c78f391c9da4731fc5092f8312c8b07a4
Anderson Tavares <andersonrochatavares@gmail.com>
2019-07-24 16:35:01 -0300

    returns more statistics & accepts a folder list

--- analysis/average_score.py
+++ analysis/average_score.py
@@ -4,67 +4,62 @@ import argparse
 import os
 
 
-def score(filename, player_index):
+def stats(filename, player_index):
     """
-    Calculates the score (victories + 0.5*draws) from the results in a given .csv file
+    Calculates the statistics (win, loss, draws) from the results in a given .csv file
     Victories are matches with result 0, draws are matches with result -1 (losses have result 1)
     :param filename:
     :param player_index
-    :return:
+    :return: wins, draws, losses, score (wins + 0.5*draws) and %score (according to the number of matches) 
     """
     df = pd.read_csv(filename)
     # renames to access with df.result
     df.rename(columns={'#result': 'result'}, inplace=True)
 
-    # score =  number of victories + 0.5 number of draws
-    victories = df[df.result == player_index].count()['result']
+    # score =  number of wins + 0.5 number of draws
+    wins = df[df.result == player_index].count()['result']
     draws = df[df.result == -1].count()['result']
-    return victories + 0.5 * draws
+    losses = df[df.result == (1 - player_index)].count()['result']
+    
+    score = wins + 0.5 * draws
+    
+    return wins, draws, losses, score, score / (wins + draws + losses) #returns the score and its percent
 
 
-def average_score(basedir, initial_rep, final_rep, opponent, position):
+def average_score(directories, opponent, position):
     """
     Calculates the average score in a series of repeated test matches
     against an opponent in a given map
-    :param basedir: base directory of the experiment (the one storing rep0,rep1,etc)
-    :param initial_rep:
-    :param final_rep:
+    :param directories: base directory of the experiment (the one storing rep0,rep1,etc)
     :param opponent:
     :param position: player position (0 or 1)
     :return:
     """
     filename = 'test-vs-%s_p%d.csv' % (opponent, position) if position is not None else 'test-vs-%s.csv' % opponent
-    files = [os.path.join(basedir, 'rep%d' % rep, filename) for rep in range(initial_rep, final_rep + 1)]
+    files = [os.path.join(directory,  filename) for directory in directories]
     #print([score(f) for f in files])
-    return np.mean([score(f, position) for f in files])
+    
+    #print ( [stats(f, position) for f in files])
+    return np.mean([stats(f, position) for f in files], axis=0)
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-        description='Calculates the average score in a series of repeated test matches '
-                    'against an opponent in a given map.\n'
+        description='Calculates the statistics in a series of experiments '
+                    'against an opponent and returns them.\n'
+                    'The returned statistics are wins,draws,losses,score and %score. '
                     'In an experiment, the score is #victories + 0.5 * #draws. '
-                    'The script looks in directories with the pattern:\n '
-                    'basedir/selfplay-[map]/rep[%d]/test-vs-[opponent].csv, where %d is in [initial_rep, final_rep].\n'
+                    'The script looks for files named test-vs-[opponent]_p%d.csv in each of the directories, ' 
+                    'where %d is the player index (0 or 1)\n'
                     'Usage example:\n'
-                    'python3 analysis/average_score.py -b results -o LightRush -i 0 -f 9 -m basesWorkers8x8\n'
+                    'python3 analysis/average_score.py results/rep* -o LightRush\n'
                     'For multiple opponents:\n'
-                    'for o in {"HeavyRush","RangedRush","WorkerRush","LightRush"}; do echo $o; python3 analysis/average_score.py -b results -o $o -i 0 -f 9 -m basesWorkers8x8; done'
+                    'for o in {"HeavyRush","RangedRush","WorkerRush","LightRush"}; do echo $o; python3 analysis/average_score.py results/rep* -o $o; done'
     )
 
     parser.add_argument(
-        'basedir', help='base directory of the experiment (the one storing rep0,rep1,etc)'
-    )
-
-    parser.add_argument(
-        '-i', '--initial-rep', type=int, required=True,
-        help='Number of initial repetition.'
-    )
-
-    parser.add_argument(
-        '-f', '--final-rep', type=int, required=True,
-        help='Number of the final repetition.'
+        'directories', help='list of directories to analyze (you can use terminal wildcards)', nargs="+"
     )
 
     parser.add_argument(
@@ -79,5 +74,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    print(average_score(args.basedir, args.initial_rep, args.final_rep, args.opponent, args.position))
+    print("wins, draws, losses, score, %score")
+    print(",".join([str(x) for x in average_score(args.directories, args.opponent, args.position)] ))
 
