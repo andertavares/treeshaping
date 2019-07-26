f8074a6cb83baf166aace3031426210536d2438e
anderson <andersonrochatavares@gmail.com>
2019-07-26 16:19:22 -0300

    utility to find the next available rep# (must think about random seeds)

--- src/utils/FileNameUtil.java
+++ src/utils/FileNameUtil.java
@@ -23,4 +23,17 @@ public class FileNameUtil {
 		
 		return filename;
 	}
+
+	/**
+	 * Finds the next 'number' such that a file named namenumber does not exist.
+	 * Useful for directories (e.g. rep0, rep1, ...) or files without extensions
+	 * @param name
+	 * @return
+	 */
+	public static int nextAvailableRepNumber(String workingDir) {
+		int num; //declared out of the for to persist for the return
+		for (num = 0; new File(String.format("%s/rep%d", workingDir, num)).exists(); num++) {} //yes, an empty for
+		
+		return num;
+	}
 }
