1698f4795952fb6fc4c86366b181af4b90cd5d81
Anderson Tavares <andersonrochatavares@gmail.com>
2019-07-30 13:17:44 -0300

    A3N allows the specification of a unrestricted unit selection policy

--- src/players/A3N.java
+++ src/players/A3N.java
@@ -19,14 +19,24 @@ public class A3N extends CmabAssymetricMCTS {
 	
 	
 	/**
-	 * Instantiates A3N by calling the superclass constructor
-	 * with appropriate parameters
+	 * Instantiates A3N with a default unrestricted unit selection policy.
 	 * @param types
 	 */
 	public A3N (UnitTypeTable types) {
+		this(types, "ManagerClosestEnemy", 1);
+	}
+	
+	/**
+	 * Instantiates A3N specifying the unrestricted unit selection policy, which consists 
+	 * of a strategy (the name of a manager) and a number
+	 * @param types
+	 * @param unrestrictedStrategy
+	 * @param unrestrictedNumber
+	 */
+	public A3N (UnitTypeTable types, String unrestrictedStrategy, int unrestrictedNumber) {
 		super(
             100, -1, 100, 8, 0.3f, 0.0f, 0.4f, 0, new RandomBiasedAI(types),
-            new SimpleSqrtEvaluationFunction3(), true, types, "ManagerClosestEnemy", 1, 
+            new SimpleSqrtEvaluationFunction3(), true, types, unrestrictedStrategy, unrestrictedNumber, 
             Arrays.asList(new LightRush(types), new RangedRush(types), new HeavyRush(types)),
             "A3N"
         );
