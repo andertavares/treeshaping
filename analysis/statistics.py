916e7c6f3cd0796153cc9e03d27628aeedc76fe4
Anderson Tavares <andersonrochatavares@gmail.com>
2019-07-31 13:45:21 -0300

    no need to specify opponent anymore, just the file name directly

--- analysis/statistics.py
+++ analysis/statistics.py
@@ -28,22 +28,20 @@ def stats(filename, player_index):
     return wins, draws, losses, num_matches, score,  100 * score / num_matches #returns the score and its percent
 
 
-def average_score(directories, opponent, position):
+def average_score(files, position):
     """
     Calculates the average score in a series of repeated test matches
-    against an opponent in a given map
-    :param directories: base directory of the experiment (the one storing rep0,rep1,etc)
-    :param opponent:
+    :param files: csv files with results
     :param position: player position (0 or 1)
     :return:
     """
-    filename = 'test-vs-%s_p%d.csv' % (opponent, position) if position is not None else 'test-vs-%s.csv' % opponent
-    files = [os.path.join(directory,  filename) for directory in directories]
+    #filename = 'test-vs-%s_p%d.csv' % (opponent, position) if position is not None else 'test-vs-%s.csv' % opponent
+    #files = [os.path.join(directory,  filename) for directory in files]
     #print([score(f) for f in files])
     
     #print ( [stats(f, position) for f in files])
-    player_index = position if position is not None else 0
-    return np.mean([stats(f, player_index) for f in files], axis=0)
+    #player_index = position if position is not None else 0
+    return np.mean([stats(f, position) for f in files], axis=0)
 
 
 if __name__ == '__main__':
@@ -53,7 +51,7 @@ if __name__ == '__main__':
                     'against an opponent and returns them.\n'
                     'The returned statistics are wins,draws,losses,#matches,score and %score. '
                     'In an experiment, the score is #victories + 0.5 * #draws. '
-                    'The script looks for files named test-vs-[opponent]_p%d.csv in each of the directories, ' 
+                    'The script looks for files named test-vs-[opponent]_p%d.csv in each of the files, ' 
                     'where %d is the player index (0 or 1)\n'
                     'Usage example:\n'
                     'python3 analysis/average_score.py results/rep* -o LightRush\n'
@@ -62,16 +60,16 @@ if __name__ == '__main__':
     )
 
     parser.add_argument(
-        'directories', help='list of directories to analyze (you can use terminal wildcards)', nargs="+"
+        'files', help='list of files to analyze (you can use terminal wildcards)', nargs="+"
     )
 
-    parser.add_argument(
-        '-o', '--opponent', required=True,
-        help='Opponent name.'
-    )
+#     parser.add_argument(
+#         '-o', '--opponent', required=True,
+#         help='Opponent name.'
+#     )
     
     parser.add_argument(
-        '-p', '--position', required=False, type=int, choices=[0,1],
+        '-p', '--position', required=False, type=int, choices=[0,1], default=0
         help='Learning agent position (0 or 1)'
     )
     
@@ -87,5 +85,5 @@ if __name__ == '__main__':
     if args.explain:
         print("wins,draws,losses,matches,score,%score")
         
-    print(",".join([str(x) for x in average_score(args.directories, args.opponent, args.position)] ))
+    print(",".join([str(x) for x in average_score(args.files, args.opponent, args.position)] ))
 
