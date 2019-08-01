4cbce079b75b840df25f0040c5abe7bcd598940a
Anderson Tavares <andersonrochatavares@gmail.com>
2019-08-01 14:12:51 -0300

    improved script to analyse A3N vs A1N

--- scripts/analyze_a3n-vs-a1n.sh
+++ scripts/analyze_a3n-vs-a1n.sh
@@ -2,11 +2,23 @@
 
 # executes analysis/statistics.py to the results of a3n-vs-a1n if they follow a specific directory structure
 
+# tests A3N either as player 0 or 1, according to what the user specified in parameter $1
 
-for m in {16x16/TwoBasesBarracks16x16.xml,16x16/basesWorkers16x16A.xml,24x24/basesWorkers24x24A.xml,32x32/basesWorkers32x32A.xml,8x8/basesWorkers8x8A.xml,BWDistantResources32x32.xml,BroodWar/"(4)BloodBath.scmB.xml",DoubleGame24x24.xml,NoWhereToRun9x8.xml}; do 
+file="A3N-vs-A1N.csv" # assumes A3N as player 0
+
+if [[ $1 = "1" ]]; then
+	file="A1N-vs-A3N.csv"
+fi
+	
+echo "map,strategy,units,wins,draws,losses,matches,score,%score"
+
+for m in {TwoBasesBarracks16x16,basesWorkers16x16A,basesWorkers24x24A,basesWorkers32x32A,basesWorkers8x8A,BWDistantResources32x32,"(4)BloodBath.scmB",DoubleGame24x24,NoWhereToRun9x8}; do 
 	for s in {CE,FC,FE,AV+,HP-,HP+}; do 
 		for u in {0..3}; do 
-			echo "$m,$s,$u,`python3 analysis/statistics.py results/$m/s$s-u$u/{A3N-vs-A1N,A1N-vs-A3N}.csv`"
+			path=results/$m/s$s-u$u/$file
+			if [ -f $path ]; then
+				echo "$m,$s,$u,`python3 analysis/statistics.py $path`"
+			fi
 		done
 	done
 done
