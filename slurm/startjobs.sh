00aacf4f767ac4af27e51fb60ec132ae5df7b9df
Anderson Rocha Tavares <andersonrochatavares@gmail.com>
2019-08-10 10:58:18 -0300

    updated slurm scripts

--- slurm/startjobs.sh
+++ slurm/startjobs.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-	echo "Please inform the number of jobs to launch"
+if [ "$#" -ne 2 ]; then
+	echo "Please inform the initial and final job number."
 	exit
 fi
 
 #Inicia as execucoes
-for i in $(seq 0 $((${1}-1))); do
+for i in $(seq ${1} ${2}); do
 	echo "Starting $i"
-	#sleep 3 && echo "hi" >> "logs/job$i.txt" &
+	#sleep 3 && echo "hi" >> "logs/job$i.txt" & # use this line to test (toggle comments with this and the one below)
 	python3 filejobclient.py jobqueue >> "logs/job$i.txt" &
-	sleep 1 # 1 second interval to prevent race conditions
+
+	#sleep 1 # 1 second interval to prevent race conditions
 done
 wait
 
