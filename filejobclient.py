9cc808c67b1ef6ec5123ae00c3cb980018368514
anderson <andersonrochatavares@gmail.com>
2019-08-10 10:40:43 -0300

    new file job client  (hopefully more robust)

--- filejobclient.py
+++ filejobclient.py
@@ -3,13 +3,18 @@
 import os.path
 import time
 import argparse
-from pathlib import Path
 import subprocess
+from contextlib import contextmanager
+from datetime import datetime, timedelta
 
 
 class Client(object):
 
     def __init__(self, basedir):
+        """
+        Instantiates the file job client
+        :param basedir: directory where todo.txt is located (a simple text file with one command per line)
+        """
         self.job_command = None
         self.job = None
         self.basedir = basedir
@@ -24,6 +29,11 @@ class Client(object):
         self.finished_jobs = 0
 
     def run(self, max_jobs=0):
+        """
+        Runs the file job client to execute a number of jobs
+        :param max_jobs: number of jobs to execute (0 = unlimited)
+        :return:
+        """
 
         while True:
             # if I have finished as many jobs as I want, halt.
@@ -33,7 +43,7 @@ class Client(object):
 
             # if job is currently running, checks if it has finished
             if self.job is not None:
-                if self.job.poll() is not None: # terminated!
+                if self.job.poll() is not None:  # terminated!
                     self.mark_finished()
 
                 else:  # job is running, wait a bit
@@ -59,39 +69,40 @@ class Client(object):
                     self.attempts = 0
 
     def find_job(self):
-        self.lock()
+        """
+        Tries to start the job at the first line of todo.txt.
+        If successful, moves the job to doing.txt
+        :return:
+        """
+        with self.lock():
+            # grabs a job
+            todo_handler = open(self.todo, 'r')
+            first_line = todo_handler.readline(4096).strip()
+            if first_line is not None and first_line != '':
 
-        # grabs a job
-        todo_handler = open(self.todo, 'r')
-        first_line = todo_handler.readline(4096).strip()
-        if first_line is not None and first_line != '':
+                # there's a valid job in the file, retrieves and runs it
+                self.job_command = first_line
 
-            # there's a valid job in the file, retrieves and runs it
-            self.job_command = first_line
+                print("Starting job '%s'" % self.job_command)
 
-            print("Starting job '%s'" % self.job_command)
+                # grabs the job, starts it and returns it
+                self.job = subprocess.Popen(
+                    self.job_command,
+                    shell=True
+                )
 
-            # grabs the job, starts it and returns it
-            self.job = subprocess.Popen(
-                self.job_command,
-                shell=True
-            )
-
-        todo_handler.close()
-        if self.job is not None:  # if I found a job, move it to in progress
-            self.move(self.job_command, self.todo, self.in_progress)
-
-        self.unlock()
+            todo_handler.close()
+            if self.job is not None:  # if I found a job, move it to in progress
+                self.move(self.job_command, self.todo, self.in_progress)
 
     def mark_finished(self):
-
+        """
+        Marks the job as finished (i.e.: moves it from doing.txt to done.txt)
+        :return:
+        """
         print("Job '%s' finished." % self.job_command)
-
-        self.lock()
-
-        self.move(self.job_command, self.in_progress, self.done)
-
-        self.unlock()
+        with self.lock():
+            self.move(self.job_command, self.in_progress, self.done)
 
         # updates my statistics
         self.job = None
@@ -99,7 +110,15 @@ class Client(object):
         self.finished_jobs += 1
 
     # moves a line from file1 to file2
-    def move(self, line_to_move, file_name1, file_name2):
+    @staticmethod
+    def move(line_to_move, file_name1, file_name2):
+        """
+        Moves a command (a line in a file) from a file to another
+        :param line_to_move: the line to be moved (must exist on the origin file)
+        :param file_name1: source file name
+        :param file_name2: destination file name
+        :return:
+        """
 
         # removes the line in file1 (opens, removes, rewrites)
         read_handler = open(file_name1, 'r')
@@ -115,17 +134,39 @@ class Client(object):
         file2_handler = open(file_name2, 'a')
         file2_handler.write('%s\n' % line_to_move)
 
-    def lock(self):
-        while os.path.exists(self.lock_file): # waits until the directory is free
-            print("Directory is locked. ")
-            time.sleep(1)
-            continue
+    @contextmanager
+    def lock(self, timeout=None, retry_time=0.5):
+        """
+        Provides mutex lock in self.basedir for safe multiprocess usage.
+        Usage:
+        with self.lock():
+            do your stuff
+        at this level of indentation, lock is released
 
-        # locks the dir
-        Path(self.lock_file).touch()
+        (adapted from https://codereview.stackexchange.com/a/150237)
 
-    def unlock(self):
-        os.remove(self.lock_file)
+        :param timeout: Seconds to wait before giving up (or None to retry indefinitely).
+        :param retry_time: Seconds to wait before retrying the lock.
+
+        """
+        if timeout is not None:
+            deadline = datetime.now() + timedelta(seconds=timeout)
+
+        while True:
+            try:
+                fd = os.open(self.lock_file, os.O_CREAT | os.O_EXCL)
+                yield   # lock acquired, user can do whatever it needs
+                break   # gets out of while True to release the lock
+            except FileExistsError:
+                if timeout is not None and datetime.now() >= deadline:
+                    raise
+                time.sleep(retry_time)
+
+        # releases the lock
+        try:
+            os.close(fd)
+        finally:
+            os.unlink(self.lock_file)
 
 
 if __name__ == '__main__':
