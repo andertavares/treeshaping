efcff246c9d079b3c2c68e4bbcc86533552a4df8
Anderson Tavares <andersonrochatavares@gmail.com>
2019-08-13 13:15:06 -0300

    adding an interval between job launches

--- slurm/startjobs.sh
+++ slurm/startjobs.sh
@@ -23,8 +23,7 @@ for i in $(seq ${1} ${2}); do
 	echo "Starting $i"
 	#sleep 3 && echo "hi" >> "logs/job$i.txt" & # use this line to test (toggle comments with this and the one below)
 	python3 filejobclient.py $queuedir >> "$queuedir/logs/job$i.txt" &
-
-	#sleep 1 # 1 second interval to prevent race conditions
+	sleep 1 # 1 second interval to help avoiding race conditions
 done
 wait
 
