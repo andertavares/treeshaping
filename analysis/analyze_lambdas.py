7b029200d289c32d5783e567406c6076bdf2b102
Anderson <andersonrochatavares@gmail.com>
2019-08-30 16:27:26 -0300

    accepting all parameters
    must check how it behaves when multiple values are specified

--- analysis/analyze_lambdas.py
+++ analysis/analyze_lambdas.py
@@ -10,54 +10,6 @@ sys.path.append('scripts') # dirty trick to do the import below
 import generate_experiments as experiments
 import commandlog
 
-'''
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description='Analyses the results of lambda tests'
-    )
-
-    parser.add_argument(
-        'basedir', help='Base directory or results',
-    )
-
-    parser.add_argument(
-        '-q', '--stdout', action='store_true', 
-        default=False,
-        help='Quiet: output to stdout rather than to a file',
-    )
-    
-    parser.add_argument(
-        '-t', '--train-matches', required=True, type=int, 
-        help='Number of train matches'
-    )
-    
-    parser.add_argument(
-        '-l', '--lambdas', help='List of lambda values', nargs='+',
-        default=[0.1, 0.3, 0.5, 0.7, 0.9]
-    )
-    
-    parser.add_argument(
-        '-s', '--strategies', help='List of strategies (comma-separated list without spaces)', 
-        default='CC,CE,FC,FE,AV-,AV+,HP-,HP+,R,M'
-    )
-    
-    parser.add_argument(
-        '-r', '--rep', help='Repetition number to analyse', type=int,
-        default=0
-    )
-    
-    parser.add_argument(
-        '--silent', help='Does not log command to the log file', action='store_true',
-    )
-    
-    parser.add_argument(
-        '-m', '--maps', help='List of maps', nargs='+',
-        default=['basesWorkers8x8', 'NoWhereToRun9x8', 'TwoBasesBarracks16x16']
-    )
-
-    return parser.parse_args()
-'''
-
 
 def raw_analysis(params): #(basedir, initial_rep, final_rep, trainmatches, maps, strategies, lambdas, stdout):
     
@@ -70,31 +22,37 @@ def raw_analysis(params): #(basedir, initial_rep, final_rep, trainmatches, maps,
             'trainmatches,map,lambda,features,strategy,position,wins,draws,losses,matches,score,%score\n'
         )
 
-        for m in params.maps:
-            for lam in params.lambdas:
-                for strat in params.strategies:
-                    path = os.path.join(
-                        params.basedir, m, 'fmaterialdistancehp_s%s_rwinlossdraw' % strat,
-                        'm%d' % params.train_matches, 'd10', 'a0.01_e0.1_g1.0_l%s' % lam,  # TODO allow custom alpha-epsilon-gamma
-                        'rep%d',  # this number will be set when building the file list
-                        'test-vs-A3N_p%d.csv' % player
-                    )
+        #for m in params.maps:
+        #    for lam in params.lambdas:
+        #        for strat in params.strategies:
+        for mapname, interval, alpha, gamma, lamda, epsilon, strats, train_opp in experiments.cartesian_product(vars(params)):
+            path = os.path.join(
+                params.basedir, mapname, 'fmaterialdistancehp_s%s_rwinlossdraw' % strats,
+                'm%d' % params.train_matches, 'd%d' % interval,
+                'a%s_e%s_g%s_l%s' % (alpha, epsilon, gamma, lamda),
+                'rep%d',  # this number will be set when building the file list
+                'test-vs-A3N_p%d.csv' % player
+            )
 
-                    file_list = [path % rep for rep in range(params.initial_rep, params.final_rep+1)]  # +1 to include the final rep
+            file_list = [path % rep for rep in range(params.initial_rep, params.final_rep+1)]  # +1 to include the final rep
 
-                    # filters out non-existent files
-                    for f in file_list:
-                        if not os.path.exists(f):
-                            print('%s does not exist' % f)  # this one always go to stdout
-                            file_list.remove(f)
+            # filters out non-existent files
+            non_existent = []
+            for f in file_list:
+                if not os.path.exists(f):
+                    print('WARNING: %s does not exist' % f)  # this one always go to stdout
+                    non_existent.append(f)
 
-                    if len(file_list) > 0:  # if there are remaining files after filtering, analyse them
-                        outstream.write('%d,%s,%s,materialdistancehp,%s,%d,%s\n' % (
-                            params.train_matches, m, lam, strat.replace(',', ' '), player,
-                            ','.join([str(x) for x in statistics.average_score(file_list, player)])
-                        ))
+            # does a set operation to remove the non existent files and then converts back to list
+            file_list = list(set(file_list) - set(non_existent))
 
-        if params.output is not None: # prevents closing sys.stdout
+            if len(file_list) > 0:  # if there are remaining files after filtering, analyse them
+                outstream.write('%d,%s,%s,materialdistancehp,%s,%d,%s\n' % (
+                    params.train_matches, mapname, lamda, strats.replace(',', ' '), player,
+                    ','.join([str(x) for x in statistics.average_score(file_list, player)])
+                ))
+
+        if params.output is not None:  # prevents closing sys.stdout
             outstream.close()
     
 
