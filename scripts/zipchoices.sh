067cc2bcc1c4484706f4618e8f1220b3cf7ec257
anderson <andersonrochatavares@gmail.com>
2019-09-06 15:22:26 -0300

    clearing comments up

--- scripts/zipchoices.sh
+++ scripts/zipchoices.sh
@@ -5,23 +5,7 @@ if [ "$#" -lt 1 ]; then
 	exit
 fi
 
-#for f in $1/*; do
-#	ext=${f##*.}
-#	echo $ext
-#	if [ "$ext" == "choices" ]; then
-#		echo $f
-#	fi
-#done
-
-#find "$1" -type f -exec sh -c '
-#  echo "$0"
-#  ext=${0##*.}
-#	if [ "$ext" == "choices" ]; then
-#		#echo "zipping $file"
-#		zip -Tmj "$0".zip $0  # -T to test, -m to remove the old file, -j to store file w/o full path
-#	fi
-#' {} \;
-
+# recursive traversal vai shopt by: https://unix.stackexchange.com/a/139369/370461
 shopt -s globstar dotglob
 for file in $1/**/*; do 
 	#echo $file
@@ -30,16 +14,4 @@ for file in $1/**/*; do
 		zip -Tmj "$file".zip $file  # -T to test, -m to remove the old file, -j to store file w/o full path
 	fi 
 done 
- 
-# the black magic of traverse via find is from https://stackoverflow.com/a/18897659/1251716
-# the following gives a 'is a directory' message, but it was the only way find worked...
-#echo "Please ignore the 'Is a directory' message."
-#find `${1}` -print0 | while IFS= read -r -d '' file 
-#do 
-#	if [ "${file##*.}" == "choices" ]; then
-#		#echo "zipping $file"
-#		zip -Tmj "$file".zip $file  # -T to test, -m to remove the old file, -j to store file w/o full path
-#	fi
-#done
-
 
