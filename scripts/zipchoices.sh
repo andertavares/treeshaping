cee238110e63af460b5a4c8a86a32258e3e79374
anderson <andersonrochatavares@gmail.com>
2019-09-06 15:19:19 -0300

    all previous attempts (commented) were working fine
    I thought they were wrong because I was looking for the results in a
    directory and executing the script in another

--- scripts/zipchoices.sh
+++ scripts/zipchoices.sh
@@ -21,16 +21,25 @@ fi
 #		zip -Tmj "$0".zip $0  # -T to test, -m to remove the old file, -j to store file w/o full path
 #	fi
 #' {} \;
- 
-# the black magic of traverse via find is from https://stackoverflow.com/a/18897659/1251716
-# the following gives a 'is a directory' message, but it was the only way find worked...
-echo "Please ignore the 'Is a directory' message."
-find `${1}` -print0 | while IFS= read -r -d '' file 
-do 
+
+shopt -s globstar dotglob
+for file in $1/**/*; do 
+	#echo $file
 	if [ "${file##*.}" == "choices" ]; then
 		#echo "zipping $file"
 		zip -Tmj "$file".zip $file  # -T to test, -m to remove the old file, -j to store file w/o full path
-	fi
-done
+	fi 
+done 
+ 
+# the black magic of traverse via find is from https://stackoverflow.com/a/18897659/1251716
+# the following gives a 'is a directory' message, but it was the only way find worked...
+#echo "Please ignore the 'Is a directory' message."
+#find `${1}` -print0 | while IFS= read -r -d '' file 
+#do 
+#	if [ "${file##*.}" == "choices" ]; then
+#		#echo "zipping $file"
+#		zip -Tmj "$file".zip $file  # -T to test, -m to remove the old file, -j to store file w/o full path
+#	fi
+#done
 
 
